f53e195c02bdbe4bdabdcd3a49d843406cbe4251
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-09-16 14:53:27 +0100

    Update 75 files

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import {initFullscreen} from "./fullscreen";
 createRoot(document.getElementById("root")).render(<App/>);
+initFullscreen();
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
